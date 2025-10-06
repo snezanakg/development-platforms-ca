dbff74cab9989763b8b958d4da560935fce9ae3f
snezanakg <142974537+snezanakg@users.noreply.github.com>
2025-10-06 13:42:38 +0200

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 const SUPABASE_URL = 'https://bncbcsqohzxnajkqigad.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJuY2Jjc3FvaHp4bmFqa3FpZ2FkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg1MzQ3NDgsImV4cCI6MjA0NDEwMDc0OH0.6wGcT6FzRCjS7S8fZbJqz2P7b9gCeHzlRPPwSCiXoyY'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJuY2Jjc3FvaHp4bmFqa3FpZ2FkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3MDAwMjcsImV4cCI6MjA3NDI3NjAyN30.VSUuRdL69MPtsE0R9C4gUBWq2LWDQsFGPjR6DwDfc18'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
